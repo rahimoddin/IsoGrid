fa8c47b384852262b0427dfacff342071a833f07
Rahimoddin Naikwade <Rahimoddin.Naikwade@externals.adidas-group.com>
2018-10-05 18:04:04 +0530

    ui changes for popup and dynamic cols generation

--- show-grid.js
+++ show-grid.js
@@ -1,129 +1,32 @@
-/*
-Grid overlay bookmarklet
-
-Shows a responsive grid overlay on top of an existing page. Based on the Clarins v3 grid
-which has a container maximum width, columns with fixed margins (gutters), variable
-number of columns between layouts, and 3 layouts: "small" (mobile), "medium" (tablet)
-and "large" (desktop).
-
-Note this is independent of the grid CSS in the site, and as such is intended as a visual
-test tool to see if elements are aligned to the grid.
-
-This is coupled with sass/show-grid.scss and dd-grid-bookmarket.html which adds the bookmarklet.
-*/
+/**
+ * Creates markup for grid ovelay
+ * cae-grid-test - parent container
+ * cae-grid-overlay - container for columns
+ * cae-ovelay-col - column 
+ * cae-inner inner area of the column
+ */
 var div = document.createElement("div");
 div.className = "cae-grid-test";
+var numcols = 12;
+var overlay = document.createElement('div');
+overlay.className = 'cae-grid-overlay';
+
+for(i=0; i< numcols; i++) {
+	var gridcol = document.createElement("div");
+	gridcol.className = 'cae-overlay-col';
+	
+	var inner = document.createElement("div");
+	inner.className ='cae-inner';
+	
+	gridcol.appendChild(inner)
+	overlay.appendChild(gridcol);
+}
+
 div.innerHTML = '\
 	<!-- Show a grid over lay (transparent)--> \
 	<input type="checkbox" id="cae-grid-toggle" class="cae-grid-toggle" /> \
-	<label for="cae-grid-toggle">Toggle CAE Grid</label> \
-	<div class="cae-grid-overlay"> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-		<div class="cae-overlay-col"> \
-			<div class="cae-inner"> \
-			</div> \
-		</div> \
-	</div><!-- grid-overlay --> \
+	<label for="cae-grid-toggle">Toggle Grid</label> \
 ';
+
 document.getElementsByTagName('body')[0].appendChild(div);
-//console.log('url:: '+ chrome.runtime.getURL());
-/* Add styling *///
-//var path = chrome.extension.getURL('adidas-grid.css');
-//chrome.tabs.insertCSS(null, {file:"adidas-grid.css"});
-//console.log('path:'+path);
-//var s = document.createElement("link");
-////s.rel = "stylesheet";
-//s.type = "text/css";
-//s.href = path;//"css/adidas-grid.css";
-//document.getElementsByTagName('head')[0].appendChild(s);
+div.appendChild(overlay);
